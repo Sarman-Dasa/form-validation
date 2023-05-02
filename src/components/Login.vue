010609de520bc32a8e928d097745b711afc0ee38
Sarman-Dasa <sarmand@zignuts.com>
2023-05-02 19:40:08 +0530

    check user already login or not

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,25 +3,27 @@
         <div class="row">
             <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="show">
                 <strong>Invalid!</strong> User name & password
-                <button type="button" class="close btn"  @click="show=!show">
+                <button type="button" class="close btn" @click="show = !show">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="col-12 form-group">
                 <label class="col-form-label col-form-label-lg">Email <span class="text-danger">*</span></label>
-                <input type="email" v-model.trim="$v.email.$model" :class="{'is-invalid': validationStatus($v.email)}" class="form-control form-control-lg">
+                <input type="email" v-model.trim="$v.email.$model" :class="{ 'is-invalid': validationStatus($v.email) }"
+                    class="form-control form-control-lg">
                 <div v-if="!$v.email.required" class="invalid-feedback">The email field is required.</div>
                 <div v-if="!$v.email.email" class="invalid-feedback">The email is not valid.</div>
             </div>
             <div class="col-12 form-group">
                 <label class="col-form-label col-form-label-lg">Password <span class="text-danger">*</span></label>
-                <input type="password" v-model.trim="$v.password.$model" :class="{'is-invalid': validationStatus($v.password)}" class="form-control form-control-lg">
+                <input type="password" v-model.trim="$v.password.$model"
+                    :class="{ 'is-invalid': validationStatus($v.password) }" class="form-control form-control-lg">
                 <div v-if="!$v.password.required" class="invalid-feedback">The password field is required.</div>
             </div>
             <div class="col-12 form-group text-center">
                 <button class="btn btn-vue btn-lg col-4">Sign Up</button>
             </div>
-           
+
         </div>
     </form>
 </template>
@@ -55,7 +57,6 @@ export default {
             this.$v.$touch();
             if (this.$v.$pendding || this.$v.$error)
                 return;
-            this.$router.push({ path: "/" });
             this.login();
         },
         async login() {
@@ -64,22 +65,29 @@ export default {
                 localStorage.setItem("users", JSON.stringify(result.data[0]));
                 this.$v.$reset();
                 this.resetData();
+                this.$router.push({ name: 'Home' });
             }
             else {
                 this.show = true;
             }
         }
     },
+    mounted() {
+       let user = localStorage.getItem("users");
+       if(user) {
+            this.$router.push({name:"Home"});
+       }
+    }
 }
 </script>
 <style>
-.btn-vue{
+.btn-vue {
     margin-top: 5%;
     background: #53B985;
     color: #31485D;
     font-weight: bold;
 }
+
 label {
     text-align: justify;
-}
-</style>
+}</style>
