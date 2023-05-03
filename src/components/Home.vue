c89a517d66d9a402c013ba9beb452961eb334b0f
Sarman-Dasa <sarmand@zignuts.com>
2023-05-03 11:13:48 +0530

    print a user name after login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,23 @@
 <template>
-    <h2>Welcome,</h2>
+    <h2>Welcome,{{ username }}</h2>
 </template>
 
 <script>
     export default {
         name:"HomePage",
+        data() {
+            return {
+                username:null,
+            }
+        },
+        mounted() {
+            let user = JSON.parse(localStorage.getItem('users'));
+            if(user) {               
+                this.username = user.name;
+            }
+            else { 
+                this.$router.push('/login');
+            }
+        }
     }
 </script>
