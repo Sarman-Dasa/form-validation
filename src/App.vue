b891aca9f3e8c44d612234e21f90f44a2bb4478b
Sarman-Dasa <sarmand@zignuts.com>
2023-05-03 11:41:39 +0530

    add logout route and check user login or not

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@
     <div class="container-fluid h-100">
       <div class="nav">
         <router-link to="/">Home</router-link>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/registration">Add</router-link>
+        <div class="nav-rigth">
+          <router-link to="/login" v-if="show">Login</router-link>
+          <router-link to="/registration">Registration</router-link>
+          <a @click="logout()" v-if="!show">Logout</a>
+        </div>
       </div>
       <div class="row h-100">
         <div class="col-md-3 vue-bg h-100% d-flex justify-content-center align-items-center">
@@ -27,8 +30,22 @@ export default {
   data() {
     return {
       tab:"UserForm",
+      show: true,
     }
   },
+  methods: {
+    logout() {
+      localStorage.removeItem('users');
+      this.$router.push('/login');
+    }
+  },
+  updated() {
+    let user = localStorage.getItem("users");
+      if(user) {
+        this.show = false
+      }
+      else { this.show = true}
+  }
 }
 </script>
 
@@ -46,6 +63,7 @@ export default {
   width: 100%;
   background-color: #2c3e50;
   color: #fff;
+  overflow:hidden;
 }
 a {
   color: #fff;
@@ -53,9 +71,13 @@ a {
   font-size: large;
   padding: 20px 25px;
   font-weight: bold;
+  float: left;
 }
 a:hover {
   background-color: green;
   border-radius: 10px;
+}
+.nav-rigth {
+  float: right;
 }
 </style>
