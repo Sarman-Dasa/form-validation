2c5e6aa39aa2240cdafb536221ae0ba19a4d0ba5
Sarman-Dasa <sarmand@zignuts.com>
2023-05-04 19:38:23 +0530

    send data using event bus

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -3,27 +3,42 @@
         <div class="col-12 text-bg-info col-12 p-3">
             Emite Example
         </div>
-        <div class="col-12  col-12 p-3">
-            <input type="text" class="form-control mt-2" v-model="name">
+        <div class="col-6  col-6 p-3">
+            <input type="text" class="form-control col-6 mt-2" v-model="name" placeholder="name">
+        </div>
+        <div class="col-6  col-6 p-3">
+            <input type="text" class="form-control col-6 mt-2" v-model="email" placeholder="email">
         </div>
         <div class="col-12">
-            <button @click="performEmit" class="btn col-12 text-primary btn-info">Send</button>
+            <button @click="performEmit" class="btn col-6 text-primary btn-info">Send</button>
+            <button @click="performEventBus" class="btn col-6 text-primary btn-dark">Event Bus</button>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'EmiteExample',
-        data() {
-            return {
-                name:'',
-            }
+import { eventBus } from '@/event-bus';
+export default {
+    name: 'EmiteExample',
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    methods: {
+        performEmit() {
+            this.$emit('emitted_data', this.name);
         },
-        methods: {
-            performEmit() {
-                this.$emit('emitted_data', this.name);
-            }
+        performEventBus() {
+            this.$router.push('/file-upload');
+            setTimeout(() => {
+                eventBus.$emit('event_bus_emitted', {
+                    name: this.name,
+                    email: this.email
+                })
+            }, 1000)
         }
     }
+}
 </script>
