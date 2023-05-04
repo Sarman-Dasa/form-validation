7ca3691785314fa95b6b50c523679005d98ea168
Sarman-Dasa <sarmand@zignuts.com>
2023-05-04 18:48:20 +0530

    print a chaild component data using emit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,36 @@
 <template>
+  <div class="row">
     <h2>Welcome,{{ username }}</h2>
+    <h4>{{ emitted_data_text }}</h4>
+    <Child @emitted_data="getEmittedData"/>
+  </div>
 </template>
 
 <script>
+import Child from './Child.vue';
+
     export default {
-        name:"HomePage",
-        data() {
-            return {
-                username:null,
-            }
-        },
-        mounted() {
-            let user = JSON.parse(localStorage.getItem('users'));
-            if(user) {               
-                this.username = user.name;
-            }
-            else { 
-                this.$router.push('/login');
-            }
+    name: "HomePage",
+    data() {
+        return {
+            username: null,
+            emitted_data_text:''
+        };
+    },
+    mounted() {
+        let user = JSON.parse(localStorage.getItem("users"));
+        if (user) {
+            this.username = user.name;
         }
-    }
+        else {
+            this.$router.push("/login");
+        }
+    },
+    methods: {
+        getEmittedData(data) {
+            this.emitted_data_text = data;
+        }
+    },
+    components: { Child }
+}
 </script>
