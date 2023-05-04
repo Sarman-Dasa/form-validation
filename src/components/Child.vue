ce89f7c3ba14d16713c14436ded487c364c82d3b
Sarman-Dasa <sarmand@zignuts.com>
2023-05-04 18:47:56 +0530

    send a chail data to parent using emit

---
+++ src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row text-bg-light">
+        <div class="col-12 text-bg-info col-12 p-3">
+            Emite Example
+        </div>
+        <div class="col-12  col-12 p-3">
+            <input type="text" class="form-control mt-2" v-model="name">
+        </div>
+        <div class="col-12">
+            <button @click="performEmit" class="btn col-12 text-primary btn-info">Send</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'EmiteExample',
+        data() {
+            return {
+                name:'',
+            }
+        },
+        methods: {
+            performEmit() {
+                this.$emit('emitted_data', this.name);
+            }
+        }
+    }
+</script>
